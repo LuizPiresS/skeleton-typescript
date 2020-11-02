9865aee491e511bba5ae57fab08a6543ff6ac217
Luiz Pires <me@lps.dev.br>
2020-11-02 14:29:20 -0300

    chore: add eslint standard Style code

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint',
+    'eslint-plugin-import-helpers'
+  ],
+  rules: {
+    'no-useless-constructor': 'off',
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: [
+          'module',
+          '/^@shared/',
+          ['parent', 'sibling', 'index']
+        ],
+        alphabetize: { order: 'asc', ignoreCase: true }
+      }
+    ]
+  }
+}
